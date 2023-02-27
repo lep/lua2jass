a17115f04d1eb70d53d5715900f303d31e1fa60d
lep <lep.delete@googlemail.com>
2023-02-27 17:27:33 +0100

    Working DEV setup

---
+++ GNUmakefile
@@ -0,0 +1,30 @@
+RUNTIME := runtime/Ins.j runtime/Interpreter.j runtime/Table.j runtime/Value.j  
+RUNTIME += runtime/Context.j runtime/StringTable.j runtime/List.j runtime/Print.j
+RUNTIME += runtime/Types.j runtime/Builtins.j
+AUTO	:= auto/Auto.j
+
+OUT	:= $(patsubst runtime/%, out/%, $(RUNTIME))
+OUT	+= $(patsubst auto/%, out/%, $(AUTO))
+
+.PHONY: XXX
+.PHONY: check
+
+out/%.j: runtime/%.j
+	bash process.sh $^ $@ lua_ 2>/dev/null
+
+out/%.j: auto/%.j
+	bash process.sh $^ $@ lua_ 2>/dev/null
+
+check: $(OUT)
+	pjass $$commonj Blizzard.j $$(./jorder.sh $(RUNTIME) $(AUTO)) scaffold.j
+
+war3map.j: $(OUT) scaffold.j main.j
+	pjass $$commonj Blizzard.j $$(./jorder.sh $(RUNTIME) $(AUTO)) scaffold.j main.j
+	runhaskell jcat.hs $$(./jorder.sh $(RUNTIME) $(AUTO)) scaffold.j main.j > $@
+
+
+start-wc3: war3map.j
+	jhcr-start "$<"
+
+update-wc3: war3map.j
+	jhcr-update "$<"
--- flake.nix
+++ flake.nix
@@ -6,17 +6,30 @@
         pjass.url = "github:lep/pjass";
         pjass.inputs.nixpkgs.follows = "nixpkgs";
         pjass.inputs.flake-utils.follows = "flake-utils";
+
+        common-j.url = "github:lep/common-j";
+        common-j.inputs.nixpkgs.follows = "nixpkgs";
+        common-j.inputs.flake-utils.follows = "flake-utils";
+
+	wc3.url = "git+file:/Users/lep/dev/wc3-mapping";
+        wc3.inputs.nixpkgs.follows = "nixpkgs";
+        wc3.inputs.flake-utils.follows = "flake-utils";
+	wc3.inputs.common-j.follows = "common-j";
     };
 
-    outputs = { self, nixpkgs, flake-utils, pjass }:
+    outputs = { self, nixpkgs, flake-utils, pjass, common-j, wc3 }:
 	flake-utils.lib.eachDefaultSystem (system:
             let pkgs = import nixpkgs { inherit system; };
                 pjass-drv = pjass.defaultPackage.${system};
+		#wc3-shell = wc3.devShell.${system};
+		wc3-drv = wc3.packages.${system};
 
                 ghcPackages = pkgs.haskellPackages.ghcWithPackages (ps: [
                     ps.language-lua
 		    ps.aeson
 		    ps.optparse-applicative
+		    ps.megaparsec
+		    ps.parser-combinators
                 ]);
 
 		pythonPackages = pkgs.python3.withPackages( ps: [
@@ -25,17 +38,24 @@
 
             in {
 		devShell = pkgs.mkShell {
+		    env = {
+		        commonj = "${common-j}/common.j";
+		    };
 		    shellHook = ''
-			function lli {
-			    runhaskell compile.hs $1 | python interpreter.py /dev/stdin
-			}
+		        function lli {
+		            runhaskell compile.hs $1 | python interpreter.py /dev/stdin
+		        }
 		    '';
 		    buildInputs = [
 			pythonPackages
                         ghcPackages
 			pkgs.lua5_3_compat
 			pkgs.jq
+			pkgs.shellcheck
                         pjass-drv
+			wc3-drv.jhcr-start
+			wc3-drv.jhcr-update
+			wc3-drv.wc3
 		    ];
 		};
             }
